b662cd0e95e08b68cc4eca8f00cf29c9a6daa83c
eslaurente <e.s.laurente@gmail.com>
2020-06-02 23:49:01 -0700

    Added index page and controller

---
+++ src/main/java/laurente/reference/recipeapp/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package laurente.reference.recipeapp.domain;
+
+public enum Difficulty {
+    EASY, MEDIUM, KIND_OF_HARD, HARD
+}
--- src/main/java/laurente/reference/recipeapp/domain/Recipe.java
+++ src/main/java/laurente/reference/recipeapp/domain/Recipe.java
@@ -22,6 +22,9 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
@@ -103,6 +106,14 @@ public class Recipe {
 
     public void setImage(Byte[] image) {
         this.image = image;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
     public Notes getNotes() {
