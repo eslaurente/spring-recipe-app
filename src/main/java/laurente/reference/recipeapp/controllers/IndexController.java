130890bac5dfeb1066447d5f2ef3e78743fd2012
eslaurente <e.s.laurente@gmail.com>
2020-06-07 23:39:42 -0700

    Added query methods

--- src/main/java/laurente/reference/recipeapp/controllers/IndexController.java
+++ src/main/java/laurente/reference/recipeapp/controllers/IndexController.java
@@ -1,14 +1,35 @@
 package laurente.reference.recipeapp.controllers;
 
+import laurente.reference.recipeapp.domain.Category;
+import laurente.reference.recipeapp.domain.UnitOfMeasure;
+import laurente.reference.recipeapp.repositories.CategoryRepository;
+import laurente.reference.recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("")
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"/", "/index", "index.html"})
     public String getIndexPage() {
+        Optional<Category> category = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        category.ifPresent(v ->  System.out.printf("Category ID: %s\n", v.getId()));
+        unitOfMeasure.ifPresent(v -> System.out.printf("UoM ID: %s\n", v.getId()));
+
         return "index";
     }
 }
